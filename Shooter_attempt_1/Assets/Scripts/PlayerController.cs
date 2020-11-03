4841b409eecabce34796c17785021428f1b15111
Alexis0900 <uleualexandru@gmail.com>
2020-11-03 16:09:04 +0200

    Movement + Aiming
    Removed the bad attempts and added (hopefully) the main thing we will be working on

--- Shooter_attempt_1/Assets/Scripts/PlayerController.cs
+++ Shooter_attempt_1/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,17 +12,50 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveInput;
     private Vector3 moveVelocity;
 
+    public FixedJoystick leftStick;
+    public FixedJoystick rightStick;
+
+    public GameObject lJoystick;
+    public GameObject rJoystick;
+
     // Start is called before the first frame update
     void Start()
     {
-        myRigidBody = GetComponent<Rigidbody>();   
+        myRigidBody = GetComponent<Rigidbody>();
+
+        lJoystick = GameObject.FindWithTag("leftJoystick");
+        leftStick = lJoystick.GetComponent<FixedJoystick>();
+
+        rJoystick = GameObject.FindWithTag("rightJoystick");
+        rightStick = rJoystick.GetComponent<FixedJoystick>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        
+        //moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        moveInput = new Vector3(leftStick.Horizontal, 0f, leftStick.Vertical);
         moveVelocity = moveInput * moveSpeed;
+
+
+        //transform.LookAt(new Vector3(rightStick.Horizontal*150, transform.position.y, rightStick.Vertical*150));
+
+
+        //frameMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //Quaternion rotation = Quaternion.LookRotation(new Vector3(rightStick.Horizontal, transform.position.y, rightStick.Vertical));
+        //transform.LookAt = rotation;
+
+        if(Mathf.Abs(rightStick.Horizontal) >= 0.2f || Mathf.Abs(rightStick.Vertical) >= 0.2f)
+        {
+            float heading = Mathf.Atan2(rightStick.Horizontal, rightStick.Vertical);
+            transform.rotation = Quaternion.Euler(0f, heading * Mathf.Rad2Deg, 0f);
+        }
+
+        
+
     }
 
     void FixedUpdate()
