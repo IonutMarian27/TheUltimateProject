702ad604f2645a6050a6c6ed07ba56825a8c5d7d
Alexis0900 <uleualexandru@gmail.com>
2020-11-04 10:46:11 +0200

    added firing mechanics
    also updated the joystick link

--- Shooter_attempt_1/Assets/Scripts/PlayerController.cs
+++ Shooter_attempt_1/Assets/Scripts/PlayerController.cs
@@ -15,34 +15,38 @@ public class PlayerController : MonoBehaviour
     public FixedJoystick leftStick;
     public FixedJoystick rightStick;
 
-    public GameObject lJoystick;
-    public GameObject rJoystick;
+    // -- old way of detecting the joysticks
+    //public GameObject lJoystick;
+    //public GameObject rJoystick;
+
+    public GunController theGun;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody>();
 
-        lJoystick = GameObject.FindWithTag("leftJoystick");
-        leftStick = lJoystick.GetComponent<FixedJoystick>();
 
-        rJoystick = GameObject.FindWithTag("rightJoystick");
-        rightStick = rJoystick.GetComponent<FixedJoystick>();
+        // -- old way of detecting the joysticks
+
+        //lJoystick = GameObject.FindWithTag("leftJoystick");
+        //leftStick = lJoystick.GetComponent<FixedJoystick>();
+
+        //rJoystick = GameObject.FindWithTag("rightJoystick");
+        //rightStick = rJoystick.GetComponent<FixedJoystick>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // moving with keys
         //moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 
         moveInput = new Vector3(leftStick.Horizontal, 0f, leftStick.Vertical);
         moveVelocity = moveInput * moveSpeed;
 
-
         //transform.LookAt(new Vector3(rightStick.Horizontal*150, transform.position.y, rightStick.Vertical*150));
-
 
         //frameMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         //Quaternion rotation = Quaternion.LookRotation(new Vector3(rightStick.Horizontal, transform.position.y, rightStick.Vertical));
@@ -52,6 +56,12 @@ public class PlayerController : MonoBehaviour
         {
             float heading = Mathf.Atan2(rightStick.Horizontal, rightStick.Vertical);
             transform.rotation = Quaternion.Euler(0f, heading * Mathf.Rad2Deg, 0f);
+
+            theGun.isFiring = true;
+
+        } else
+        {
+            theGun.isFiring = false;
         }
 
         
