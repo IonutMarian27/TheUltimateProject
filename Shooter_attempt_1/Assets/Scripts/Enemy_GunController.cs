8f431a3470426448233914074522ff949336c5f9
IonutMarian <48709347+IonutMarian27@users.noreply.github.com>
2020-11-22 21:32:06 +0200

    New enemy and basic movement AI added

---
+++ Shooter_attempt_1/Assets/Scripts/Enemy_GunController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Enemy_GunController : MonoBehaviour
+{
+
+    public bool isFiring = true;
+    
+    public BulletController bullet;
+    public float bulletSpeed;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isFiring)
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter <= 0)
+            {
+                shotCounter = timeBetweenShots;
+                
+                BulletController newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as BulletController;
+                newBullet.speed = bulletSpeed;
+
+            }
+        } 
+        else
+        {
+            shotCounter = 0;
+        }
+    }
+}
